c42ca78da37667211c9da6805e39bec28ca98f1a
markskwid <bascal.markponce.f.1015@gmail.com>
2023-03-01 19:45:38 +0800

    Frontend Challenge

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ function App() {
           <div className="flex flex-col justify-center items-center bg-gradient-to-b from-[#7857FF] to-[#2E2BE9] rounded-b-3xl m-0 py-3 px-8 md:rounded-3xl lg:w-1/2 lg:rounded-2xl lg:px-14">
             <h1 className="font-semibold text-slate-200 text-center text-lg lg:text-2xl">
               Your Result
-              <div className="cirle p-7 w-36 bg-gradient-to-b from-[#4E21CA]/90 to-[#2421CA]/10 rounded-full text-center my-3 mx-auto lg:w-48 lg:p-12 lg:my-8 lg:flex lg:flex-col justify-center items-center">
-                <p className="text-6xl text-white font-bold lg:text-7xl">
-                  76
-                  <span className="block text-slate-400 text-base">of 100</span>
-                </p>
-              </div>
-              <p className="text-2xl text-white mb-2 lg:text-3xl">Great</p>
-              <p className="text-slate-400 text-base text-center font-extralight mb-5">
-                You scored higher than 65% of the people who have taken these
-                tests.
-              </p>
             </h1>
+            <div className="cirle p-7 w-36 bg-gradient-to-b from-[#4E21CA]/90 to-[#2421CA]/10 rounded-full text-center my-3 mx-auto lg:w-48 lg:p-12 lg:my-8 lg:flex lg:flex-col justify-center items-center">
+              <p className="text-6xl text-white font-bold lg:text-7xl">
+                76
+                <span className="block text-slate-400 text-base">of 100</span>
+              </p>
+            </div>
+            <p className="text-2xl text-white mb-2 lg:text-3xl lg:font-semibold">Great</p>
+            <p className="text-slate-400 text-base text-center font-extralight mb-5">
+              You scored higher than 65% of the people who have taken these
+              tests.
+            </p>
           </div>
 
           <div className="box-wrapper m-0 flex flex-col justify-start items-start p-4 w-full lg:w-1/2 lg:p-7 ">
